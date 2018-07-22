8fcb68a12d8523db6844f0bfd2c0b1e520e04cf8
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-22 23:55:29 +0700

    Update Solution.java

--- 30 Days Of Code/Day 08 - Dictionaries and Maps/Solution.java
+++ 30 Days Of Code/Day 08 - Dictionaries and Maps/Solution.java
@@ -1,1 +1,32 @@
+// Code by Lien Bui
 
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        Map <String, Integer> newmap = new HashMap<String, Integer>();
+        
+        for ( int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            
+            newmap.put(name, phone);
+        }
+        
+        while(in.hasNext()){
+            String s = in.next();
+            
+            if(newmap.containsKey(s)){
+                System.out.println(s+"="+newmap.get(s));
+            }else{
+                System.out.println("Not found");
+            }
+        }
+        
+        in.close();
+    }
+}
