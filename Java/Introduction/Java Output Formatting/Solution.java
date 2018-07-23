740754d58c88c56fd8eaa9edb97fc8e3d46d0ca4
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-23 17:49:31 +0700

    Create Solution.java

---
+++ Java/Introduction/Java Output Formatting/Solution.java
@@ -0,0 +1,26 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                
+                System.out.printf("%-15s%03d\n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
