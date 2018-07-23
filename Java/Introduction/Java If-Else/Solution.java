5ecf6507cd77f2fa2d9b53a31f42ced35c07a982
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-23 17:41:45 +0700

    Create Solution.java

---
+++ Java/Introduction/Java If-Else/Solution.java
@@ -0,0 +1,34 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        
+        if( N % 2 == 1)
+            System.out.println("Weird");
+        else if( N>= 2 && N <= 5)
+            System.out.println("Not Weird");
+        else if ( N >= 6 && N <= 20)
+            System.out.println("Weird");
+        else
+            System.out.println("Not Weird");
+    }
+}
