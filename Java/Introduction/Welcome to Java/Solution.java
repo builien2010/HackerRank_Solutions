a2d5e18f253a1298d08b8660dc8283a10237a487
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-23 17:37:25 +0700

    Create Solution.java

---
+++ Java/Introduction/Welcome to Java/Solution.java
@@ -0,0 +1,12 @@
+//    Author: Lien Bui 
+//    GitHub: github.com/builien2010
+//    HackerRank: hackerrank.com/builien2010
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
