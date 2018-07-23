b9043858fcd14988ce137af8bfcd825915a10cde
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-23 17:40:27 +0700

    Create Solution.java

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,27 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+      
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        
+        scan.close();
+        
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+        
+    }
+}
