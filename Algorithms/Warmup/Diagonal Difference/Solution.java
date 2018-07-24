b0ab6bfebf187ffdbc63f408eee4f650f7a39252
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-24 18:11:41 +0700

    Create Solution.java

---
+++ Algorithms/Warmup/Diagonal Difference/Solution.java
@@ -0,0 +1,63 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+        int primary = 0;
+        int second = 0;
+        
+        for ( int i = 0; i < arr.length; i++){
+            for ( int j = 0; j < arr[0].length; j++){
+                if ( i == j)
+                    primary += arr[i][j];
+                if( (i + j)== (arr.length - 1))
+                    second += arr[i][j];
+            }         
+        }
+        
+        return Math.abs(primary - second);
+            
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
