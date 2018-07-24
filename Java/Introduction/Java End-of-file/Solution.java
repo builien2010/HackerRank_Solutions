117e2241db15649f92cfaa1c0ee959a45b83f135
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-24 17:15:15 +0700

    Create Solution.java

---
+++ Java/Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,20 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while (scan.hasNext()){
+            i++;
+            
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
