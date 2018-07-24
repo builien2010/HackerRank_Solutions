e3213cd0b9125be3c73f423f64e24a1b58ebe16c
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-24 09:05:19 +0700

    Update Solution.java

--- 30 Days Of Code/Day 06 - Let-s Review/Solution.java
+++ 30 Days Of Code/Day 06 - Let-s Review/Solution.java
@@ -1,1 +1,31 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        
+        for ( int i = 1; i <= n; i++){
+            
+            String s = scan.nextLine();
+            for ( int j = 0; j < s.length(); j = j + 2){
+                System.out.print(s.charAt(j));
+            }
+            System.out.print(" ");
+            for ( int j = 1; j < s.length(); j = j + 2){
+                System.out.print(s.charAt(j));
+            }
+            System.out.println();
+        }
+        
+    }
+}
