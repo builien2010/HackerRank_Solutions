07847aded52cb90f88d63f18b532d9b1d7c2a71d
builien2010 <40897137+builien2010@users.noreply.github.com>
2018-07-24 17:21:05 +0700

    Create Solution.java

---
+++ Java/Introduction/Java Static Initializer Block/Solution.java
@@ -0,0 +1,37 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+        static Scanner scan = new Scanner(System.in);
+        static boolean flag = true;
+        static int B = scan.nextInt();
+        static int H = scan.nextInt();
+        
+        static{
+            try{
+                if ( B <= 0 || H <= 0){
+                    flag = false;
+                    throw new Exception("Breadth and height must be positive");
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+                
+        }
+
+    public static void main(String[] args){
+		    if(flag){
+			      int area=B*H;
+			      System.out.print(area);
+		    }
+	  }//end of main
+
+}//end of class
