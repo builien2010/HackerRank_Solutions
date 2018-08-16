0976159123144b6d0ee7ace99911e847cb7754e3
Bùi Thị Liên <40897137+builien2010@users.noreply.github.com>
2018-08-17 03:33:50 +0700

    Create Solution.java

---
+++ Java/Object Oriented Programming/Java Abstract Class/Solution.java
@@ -0,0 +1,33 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.util.*;
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
+	
+}
+
+class MyBook extends Book{
+    public void setTitle(String s){
+        title = s;
+    }
+}
+
+public class Main{
+	
+	public static void main(String []args){
+	
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+		
+	}
+}
