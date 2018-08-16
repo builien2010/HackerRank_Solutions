add2e8aa9a977c837494384369668c5a0e4b15c0
Bùi Thị Liên <40897137+builien2010@users.noreply.github.com>
2018-08-17 03:41:52 +0700

    Create Solution.java

---
+++ Java/Object Oriented Programming/Java Method Overriding 2 - Super Keyword/Solution.java
@@ -0,0 +1,34 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.util.*;
+import java.io.*;
+
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+    
+    String temp=super.define_me(); 
+
+    System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
