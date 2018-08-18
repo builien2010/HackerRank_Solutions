e2c9f05d137836e9a132f765e39acd06a5646ab6
Bùi Thị Liên <40897137+builien2010@users.noreply.github.com>
2018-08-18 19:42:44 +0700

    Create Solution.java

---
+++ Java/Java Structures/Java Arraylist/Solution.java
@@ -0,0 +1,42 @@
+// Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+        
+        for ( int row = 0; row < n; row++) {
+            int d = scan.nextInt();
+            ArrayList<Integer> list = new ArrayList<>();
+            
+            for ( int col = 0; col < d; col++) {
+                list.add(scan.nextInt());
+            }
+            lists.add(list);
+        }
+        
+        int q = scan.nextInt();
+        
+        for ( int i = 1; i <= q; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            
+            ArrayList<Integer> list = lists.get(x-1);
+            
+            if(y <= list.size()){
+                System.out.println(list.get(y-1));
+            }else{
+                System.out.println("ERROR!");      
+            }    
+        }             
+    }
+}
