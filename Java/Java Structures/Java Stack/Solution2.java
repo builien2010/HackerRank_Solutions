9fbae3d183033c8d7002e3e2c9f2839f58315735
Bùi Thị Liên <40897137+builien2010@users.noreply.github.com>
2018-08-19 17:50:17 +0700

    Create Solution2.java

---
+++ Java/Java Structures/Java Stack/Solution2.java
@@ -0,0 +1,21 @@
+/ Author: Lien Bui 
+// GitHub: github.com/builien2010
+// HackerRank: hackerrank.com/builien2010
+
+import java.util.*;
+class Solution{
+	
+	public static void main(String []argh){ 
+        
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+            
+			String input=sc.next();
+            
+      while(input.length() != (input =  input.replaceAll("\\(\\)|\\[\\]|\\{\\}", "")).length());
+      System.out.println(input.isEmpty());
+     }
+		
+	}
+}
